c61e7fed94863ea5aab703ca4902df1d48a58100
Patrick Helm <ph@werbeboten.de>
2012-05-31 22:36:05 +0200

    Fixes csv_all.sh

--- csv_all.sh
+++ csv_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Generating Items*.csv ..."
+echo -e "\nGenerating Items*.csv"
 d3_mpq --csv-input extracted_mpqs/GameBalance/Items_Armor.gam  --csv-name Items_Armor csv Items
 d3_mpq --csv-input extracted_mpqs/GameBalance/Items_Legendary.gam  --csv-name Items_Legendary csv Items
 d3_mpq --csv-input extracted_mpqs/GameBalance/Items_Legendary_Other.gam  --csv-name Items_Legendary_Other csv Items
@@ -9,45 +9,45 @@ d3_mpq --csv-input extracted_mpqs/GameBalance/Items_Quests_Beta.gam  --csv-name 
 d3_mpq --csv-input extracted_mpqs/GameBalance/Items_Quests.gam  --csv-name Items_Quest csv Items
 d3_mpq --csv-input extracted_mpqs/GameBalance/Items_Weapons.gam  --csv-name Items_Weapons csv Items
 
-echo "Generating ItemTypes.csv ..."
+echo -e "\nGenerating ItemTypes.csv"
 d3_mpq --csv-input extracted_mpqs/GameBalance/ItemTypes.gam csv ItemTypes
 
-echo "Generating ItemEnhancements.csv ..."
+echo -e "\nGenerating ItemEnhancements.csv"
 d3_mpq --csv-input extracted_mpqs/GameBalance/ItemEnhancements.gam csv ItemEnhancements
 
-echo "Generating SetItemBonuses.csv ..."
+echo -e "\nGenerating SetItemBonuses.csv"
 d3_mpq --csv-input extracted_mpqs/GameBalance/SetItemBonuses.gam csv SetItemBonuses
 
-echo "Generating RareNames.csv ..."
+echo -e "\nGenerating RareNames.csv"
 d3_mpq --csv-input extracted_mpqs/GameBalance/RareNames.gam csv RareNames
 
-echo "Generating Recipes*.csv ..."
+echo -e "\nGenerating Recipes*.csv"
 d3_mpq --csv-input extracted_mpqs/GameBalance/RecipesBlacksmith.gam  --csv-name RecipesBlacksmith csv Recipes
 d3_mpq --csv-input extracted_mpqs/GameBalance/RecipesJeweler.gam  --csv-name RecipesJeweler csv Recipes
 d3_mpq --csv-input extracted_mpqs/GameBalance/RecipesMystic.gam  --csv-name RecipesMystic csv Recipes
 d3_mpq --csv-input extracted_mpqs/Recipe/ csv RecipeDetails
 
-echo "Generating AffixList.csv ..."
+echo -e "\nGenerating AffixList.csv"
 d3_mpq --csv-input extracted_mpqs/GameBalance/AffixList.gam csv AffixList
 
-echo "Generating Actor.csv ..."
+echo -e "\nGenerating Actor.csv"
 d3_mpq --csv-input extracted_mpqs/Actor/ csv Actor
 
-echo "Generating StringList.csv (enUS) ..."
+echo -e "\nGenerating StringList.csv (enUS)"
 d3_mpq --csv-input extracted_mpqs/StringList/ --locale enUS csv StringList
 
-echo "Generating StringList.csv (deDE) ..."
+echo -e "\nGenerating StringList.csv (deDE)"
 d3_mpq --csv-input extracted_mpqs/StringList/ --locale deDE csv StringList
 
-echo "Generating SocketedEffects.csv ..."
+echo -e "\nGenerating SocketedEffects.csv"
 d3_mpq --csv-input extracted_mpqs/GameBalance/SocketedEffects.gam csv SocketedEffects
 
-echo "Generating PowerFormulaTables.csv ..."
+echo -e "\nGenerating PowerFormulaTables.csv"
 d3_mpq --csv-input extracted_mpqs/GameBalance/PowerFormulaTables.gam csv PowerFormulaTables
 
-echo "Generating MonsterNames.csv ..."
+echo -e "\nGenerating MonsterNames.csv"
 d3_mpq --csv-input extracted_mpqs/GameBalance/MonsterNames.gam csv MonsterNames
 
-echo "Generating MonsterAffixes.csv ..."
+echo -e "\nGenerating MonsterAffixes.csv"
 d3_mpq --csv-input extracted_mpqs/GameBalance/MonsterAffixes.gam csv MonsterAffixes
 
